df43967e581a0e0a7af1c53b7420bf120311968b
dram0027/usr/local/bin/python3 <dram0027@student.monash.edu>
2024-12-04 09:40:20 +0530

    Simplifying and improving the methodology

---
+++ day3/day3_pt1.py
@@ -0,0 +1,17 @@
+import re
+
+def calculate_product(file_path):
+    with open(file_path, "r") as file:
+        chars_str = file.read()
+
+    # Find all matches for the mul(x, y) pattern
+    matches = re.findall(r"mul\((\d+),\s*(\d+)\)", chars_str) # only captures the numbers
+
+    total = 0 
+    for x, y in matches:
+        total += int(x) * int(y)
+
+    return total 
+
+print(calculate_product("day3/test.txt"))
+print(calculate_product("day3/day3_input.txt"))
